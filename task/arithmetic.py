4a123e3feaefd42b426d743bf51b85ee0a6432e9
lefrog06 <github.passably243@passmail.net>
2025-06-18 11:36:11 +0200

    Stage 2 OK

--- task/arithmetic.py
+++ task/arithmetic.py
@@ -1,4 +1,6 @@
-def parse_operation(arg):
+import random
+
+def parse_equation(arg):
     operands = arg.split(' ')
     x = float(operands[0])
     operation = operands[1]
@@ -10,9 +12,24 @@ def parse_operation(arg):
     elif operation == '*':
         return x * y
     return None
+#
+# calc_input  = input()
+# print(parse_operation(calc_input))
 
-calc_input  = input()
-print(parse_operation(calc_input))
+operations = ['+', '-', '*']
+def gen_equation():
+    x = random.randint(2,9)
+    y = random.randint(2,9)
+    op_index = random.randint(0,2)
+    operand = operations[op_index]
+    return f'{x} {operand} {y}'
 
+equation = gen_equation()
+print(equation)
+answer = float(input())
+if answer == parse_equation(equation):
+    print('Right!')
+else:
+    print('Wrong!')
 
 
